4b4ec4bc794a1edd3a2622d8269a55ba284804e8
Walmir Neto <wfsneto@gmail.com>
2016-03-28 02:12:23 -0300

    fix test association of user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'associations' do
-    it { is_expected.to have_many(:repositories) }
+    it { is_expected.to have_and_belong_to_many(:repositories) }
   end
 
   context 'database' do
